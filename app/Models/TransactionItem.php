be4d92b0e5adf86784037588630a1a6190d9f0b1
Muhammad Khairul Anwar <khairul240102@gmail.com>
2023-09-06 20:31:52 +0700

    first commit

---
+++ app/Models/TransactionItem.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'users_id',
+        'products_id',
+        'transactions_id',
+        'quantity',
+    ];
+
+    public function transactions() {
+        return $this->belongsTo(Transaction::class, 'transactions_id', 'id');
+    }
+
+    public function product() {
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
+}
